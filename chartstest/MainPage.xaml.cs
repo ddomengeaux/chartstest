b362920b5411c57e56f8d8f1b38de62598bcb8ae
Daniel Domengeaux <ddomengeaux@evssw.com>
2021-02-04 12:23:53 -0600

    add second chart to testing binding to series property

--- chartstest/MainPage.xaml.cs
+++ chartstest/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Syncfusion.SfChart.XForms;
 using Xamarin.Forms;
 
 namespace chartstest
@@ -20,12 +21,19 @@ namespace chartstest
     public class ViewModel
     {
         public ObservableCollection<Model> Data { get; set; }
+        public ChartSeriesCollection Series
+        {
+            get => series;
+            set => series = value;
+        }
 
         private DateTime Date = DateTime.Now.AddDays(-10);
+        private ChartSeriesCollection series;
 
         public ViewModel()
         {
             Data = new ObservableCollection<Model>();
+            Series = new ChartSeriesCollection();
 
             Random random = new Random();
 
@@ -34,6 +42,23 @@ namespace chartstest
                 Data.Add(new Model() { XValue = Date, YValue = random.Next(1, 69) });
                 Date = Date.AddMinutes(13);
             }
+
+            foreach (var batch in Data.Batch(100))
+            {
+                Series.Add(new FastLineSeries()
+                {
+                    ItemsSource = batch,
+                    XBindingPath = "XValue",
+                    YBindingPath = "YValue",
+                    Color = GetRandomColor()
+                });
+            }
+        }
+
+        public static Color GetRandomColor()
+        {
+            var rnd = new Random();
+            return Color.FromRgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
         }
     }
 
@@ -41,5 +66,34 @@ namespace chartstest
     {
         public DateTime XValue { get; set; }
         public double YValue { get; set; }
+    }
+
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> enumerable, int batchSize)
+        {
+            if (enumerable == null) throw new ArgumentNullException(nameof(enumerable));
+            if (batchSize <= 0) throw new ArgumentOutOfRangeException(nameof(batchSize));
+            return enumerable.BatchCore(batchSize);
+        }
+
+        private static IEnumerable<IEnumerable<T>> BatchCore<T>(this IEnumerable<T> enumerable, int batchSize)
+        {
+            var c = 0;
+            var batch = new List<T>();
+            foreach (var item in enumerable)
+            {
+                batch.Add(item);
+                if (++c % batchSize == 0)
+                {
+                    yield return batch;
+                    batch = new List<T>();
+                }
+            }
+            if (batch.Count != 0)
+            {
+                yield return batch;
+            }
+        }
     }
 }
